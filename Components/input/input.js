4514b5e8b6e8c7687d2cd56a6576c3ce2ce84b62
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-02-05 00:15:21 -0800

    Refactor

--- Components/input/input.js
+++ Components/input/input.js
@@ -1,17 +1,9 @@
 const emailInput = document.getElementById("inp-email");
 const passwordInput = document.getElementById("inp-password");
 const termsPolicyInput = document.getElementById("checkbox-termsPolicy");
-const submitBtn = document.getElementById("submit-btn");
-const form = document.getElementById("validation-form");
 const errMessage = document.getElementsByClassName("err-msg-container")
 
 function checkFormValidation(){
-
-    console.log("form")
-    console.log(emailInput.value, typeof emailInput.value);
-    console.log(passwordInput.value);
-    console.log(termsPolicyInput.checked);
-    console.log(errMessage[0])
 
     if(emailInput.value == ""){
         errMessage[0].style.display = "block";
