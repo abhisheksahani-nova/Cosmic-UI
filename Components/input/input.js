0ef25c478209f69bd9b8040ccd119e84d19a23c1
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-02-05 00:07:57 -0800

    Input component done

--- Components/input/input.js
+++ Components/input/input.js
@@ -3,14 +3,36 @@ const passwordInput = document.getElementById("inp-password");
 const termsPolicyInput = document.getElementById("checkbox-termsPolicy");
 const submitBtn = document.getElementById("submit-btn");
 const form = document.getElementById("validation-form");
+const errMessage = document.getElementsByClassName("err-msg-container")
 
 function checkFormValidation(){
 
     console.log("form")
-    console.log(emailInput.value);
+    console.log(emailInput.value, typeof emailInput.value);
     console.log(passwordInput.value);
     console.log(termsPolicyInput.checked);
+    console.log(errMessage[0])
+
+    if(emailInput.value == ""){
+        errMessage[0].style.display = "block";
+    }else{
+        errMessage[0].style.display = "none";
+    }
+
+    if(passwordInput.value == ""){
+        errMessage[1].style.display = "block";
+    }else{
+        errMessage[1].style.display = "none";
+    }
+
+    if(!termsPolicyInput.checked){
+        errMessage[2].style.display = "block";
+    }else{
+        errMessage[2].style.display = "none";
+    }
+
 }
+
 
 
 
