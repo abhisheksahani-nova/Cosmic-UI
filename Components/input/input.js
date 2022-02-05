3e332bb6f824a1752dbe023bde2e06edfc392ee2
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-02-04 23:33:51 -0800

    Add style for input component

---
+++ Components/input/input.js
@@ -0,0 +1,17 @@
+const emailInput = document.getElementById("inp-email");
+const passwordInput = document.getElementById("inp-password");
+const termsPolicyInput = document.getElementById("checkbox-termsPolicy");
+const submitBtn = document.getElementById("submit-btn");
+const form = document.getElementById("validation-form");
+
+function checkFormValidation(){
+
+    console.log("form")
+    console.log(emailInput.value);
+    console.log(passwordInput.value);
+    console.log(termsPolicyInput.checked);
+}
+
+
+
+
